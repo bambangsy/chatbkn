006b296913363f574f36290fbcb373650aaa5e3c
Raka Dimas Saputra <rakadimas56@gmail.com>
2024-05-21 16:31:17 +0700

    add functionality to group, conversation, user models

--- resources/js/Layouts/ChatLayout.jsx
+++ resources/js/Layouts/ChatLayout.jsx
@@ -1,19 +1,57 @@
 import { usePage } from "@inertiajs/react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const ChatLayout = ({ children }) => {
     const page = usePage();
     const conversations = page.props.conversations;
     const selectedConversation = page.props.selectedConversation;
+    const [localConversations, setLocalConversations] = useState([]);
+    const [sortedConversations, setSortedConversations] = useState([]);
     const [onlineUsers, setOnlineUsers] = useState({});
+
+    const isUserOnline = (userId) => onlineUsers[userId];
 
     console.log("conversations", conversations);
     console.log("selectedConversation", selectedConversation);
 
     useEffect(() => {
+        setSortedConversations(
+            localConversations.sort((a, b) => {
+                if (a.blocked_at && b.blocked_at) {
+                    return a.blocked_at > b.blocked_at ? 1 : -1;
+                } else if (a.blocked_at) {
+                    return 1;
+                } else if (b.blocked_at) {
+                    return -1;
+                }
+                if (a.last_message_date && b.last_message_date) {
+                    return b.last_message_date.localeCompare(
+                        a.last_message_date
+                    );
+                } else if (a.last_message_date) {
+                    return -1;
+                } else if (b.last_message_date) {
+                    return 1;
+                } else {
+                    return 0;
+                }
+            })
+        );
+    }, [localConversations]);
+
+    useEffect(() => {
+        setLocalConversations(conversations);
+    }, [conversations]);
+
+    useEffect(() => {
         Echo.join("online")
             .here((users) => {
-                console.log("here", users);
+                const onlineUsersObj = Object.fromEntries(
+                    users.map((user) => [user.id, user])
+                );
+                setOnlineUsers((prevOnlineUsers) => {
+                    return { ...prevOnlineUsers, ...onlineUsersObj };
+                });
             })
             .joining((user) => {
                 setOnlineUsers((prevOnlineUsers) => {
@@ -23,7 +61,11 @@ const ChatLayout = ({ children }) => {
                 });
             })
             .leaving((user) => {
-                console.log("leaving", user);
+                setOnlineUsers((prevOnlineUsers) => {
+                    const updateUsers = { ...prevOnlineUsers };
+                    delete updateUsers[user.id];
+                    return updateUsers;
+                });
             })
             .error((error) => {
                 console.error("error", error);
@@ -36,8 +78,24 @@ const ChatLayout = ({ children }) => {
 
     return (
         <>
-            ChatLayout
-            <div>{children}</div>
+            <div className="flex-1 w-full flex overflow-hidden">
+                <div
+                    className={`transition-all w-full sm:w-[220px] md:w-[300px] bg-slate-800 flex flex-col overflow-hidden ${
+                        selectedConversation ? "-ml-[100%] sm:ml-0" : ""
+                    }`}
+                ></div>
+                <div className="flex items-center justify-between py-2 px-3 text-xl font-medium">
+                    My Conversation
+                    <div className="tooltip tooltip-left" data-tip="Create New Group">
+                        <button className="text-gray-400 hover:text-gray-200">
+                            <PencilSquareIcon className="w-4 h-4 inline-block ml-2"/>
+                        </button>
+                    </div>
+                </div>
+                <div className="flex-1 flex flex-col overflow-hidden">
+                    {children}
+                </div>
+            </div>
         </>
     );
 };
