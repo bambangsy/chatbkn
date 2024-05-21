e9e1183438771d9191b9abff570f8fd214e4bc53
bambangsy <bambangsy28@gmail.com>
2024-05-21 10:19:47 +0700

    fix edit page

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,6 +1,4 @@
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import ChatLayout from '@/Layouts/ChatLayout';
-import { Head } from '@inertiajs/react';
 
 export default function Home({ auth }) {
     return (
